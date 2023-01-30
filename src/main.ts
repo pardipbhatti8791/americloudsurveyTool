545acf6b506c05f0efaeb746a7083ea8092d88aa
Pardip Bhatti <pardip@getdefault.in>
2023-01-30 15:20:09 +0530

    swagger implemenation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Authentication Authorization')
+    .setDescription(
+      'In this app we are handling authentication, authorization, refreshtoken',
+    )
+    .setVersion('1.0')
+    .addTag('AuthApp')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
